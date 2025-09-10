904f19b3e47f79a1c688062da719e2f2f1fdf62a
Elizabeth Grimes <Elizabeth.N.Grimes-1@ou.edu>
2025-09-10 15:38:55 -0500

    HW1 A&B

--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1b Grade
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Elizabeth Grimes
+// Did you seek help ? If yes, specify the helper or web link here: TA
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,33 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();  
+
+            Console.WriteLine("What is your last name?");   
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your student id?");
+            string studentId = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for homework?");
+            string homeworkGrade = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            string participationGrade = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            string quizGrade = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for midterm?");
+            string midtermGrade = Console.ReadLine();
+
+            Console.WriteLine("What is your overall percentage grade for final?");
+            string finalGrade = Console.ReadLine();
+
+            Console.WriteLine(firstName + " " + lastName + " " + studentId + " "  + finalGrade);
+
+            Console.ReadKey();
         }
     }
 }
